12d18a72f2f49d79997d2f00d310ee3be31e237d
alireza <alireza.daghigh1999@gmail.com>
2020-04-18 19:00:19 +0430

    Merge branch 'master' of /home/alireza/Desktop/HW2Havayesar with conflicts.

--- Sensors/app/src/main/java/com/example/sensors/MainActivity.java
+++ Sensors/app/src/main/java/com/example/sensors/MainActivity.java
@@ -1,95 +1,39 @@
 package com.example.sensors;
 
-import android.app.Activity;
-import android.app.TimePickerDialog;
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
-import java.util.Calendar;
-
-public class MainActivity extends Activity {
-
-    TimePicker myTimePicker;
-    Button buttonstartSetDialog;
-    TextView textAlarmPrompt;
-    TimePickerDialog timePickerDialog;
-    float need;
-
-    final static int RQS_1 = 1;
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final String TAG = "MainActivity";
+    private SensorManager sensorManager ;
+    Sensor  acceleromater;
+    TextView textView ;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        acceleromater = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        textView = findViewById(R.id.main);
+        sensorManager.registerListener(MainActivity.this , acceleromater , SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-        need = (float) 5.5;
-
-        textAlarmPrompt = (TextView) findViewById(R.id.alarmprompt);
-
-        buttonstartSetDialog = (Button) findViewById(R.id.startAlaram);
-        buttonstartSetDialog.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                textAlarmPrompt.setText("");
-                openTimePickerDialog(false);
-
-            }
-        });
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        textView.setText("cho" + event.values[2]);
+        Log.d("Tag" , "x ix" + event.values[0] + "y is" + event.values[1] + "z is " + event.values[2]);
 
     }
 
-    private void openTimePickerDialog(boolean is24r) {
-        Calendar calendar = Calendar.getInstance();
-
-        timePickerDialog = new TimePickerDialog(MainActivity.this,
-                onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE), is24r);
-        timePickerDialog.setTitle("Set Alarm Time");
-
-        timePickerDialog.show();
-
-    }
-
-    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
-
-        @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-            Calendar calNow = Calendar.getInstance();
-            Calendar calSet = (Calendar) calNow.clone();
-
-            calSet.set(Calendar.HOUR_OF_DAY, hourOfDay);
-            calSet.set(Calendar.MINUTE, minute);
-            calSet.set(Calendar.SECOND, 0);
-            calSet.set(Calendar.MILLISECOND, 0);
-
-            if (calSet.compareTo(calNow) <= 0) {
-                // Today Set time passed, count to tomorrow
-                calSet.add(Calendar.DATE, 1);
-            }
-
-            setAlarm(calSet);
-        }
-    };
-
-    private void setAlarm(Calendar targetCal) {
-
-        textAlarmPrompt.setText("\n\n***\n" + "Alarm is set "
-                + targetCal.getTime() + "\n" + "***\n");
-
-
-        Intent intent = new Intent(this, AlarmService.class);
-        intent.putExtra("millis", targetCal.getTimeInMillis());
-        intent.putExtra("need", need);
-
-        startService(intent);
-
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
-
-
